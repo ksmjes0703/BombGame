9ff1f8b307f004ae2cb355efe352d4bcad926535
ksmjes0703 <80800105+ksmjes0703@users.noreply.github.com>
2021-06-16 11:00:52 +0900

    2021_06_16_BombGame

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //이거 꼭 넣기!!!
+
+public class Button : MonoBehaviour
+{
+    public void Reset()
+    {
+        SceneManager.LoadScene("Desert");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
